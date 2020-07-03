ba565390f8d5c26b0ae71f0cf2238258be0a6e79
ali <vick12052002@gmail.com>
2020-07-03 21:01:56 +0800

    week2 ok

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  
+    var array = str.split("")
+    var newArray = []
+    for (var i = 0; i < str.length; i++){
+       newArray.push(array.pop()) 
+    }
+    console.log(newArray.join(""))
 }
-
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,30 @@
 function join(arr, concatStr) {
-  
+    var res = ""
+    if(arr.length===0){
+        return ""
+    }
+    for (var i = 0; i < arr.length; i++) {
+        if (i == arr.length - 1) {
+            res += arr[i].toString()
+        } else {
+            res += (arr[i].toString()) + concatStr
+        }
+    }
+    return res
 }
-
 function repeat(str, times) {
-  
+    let res = ""
+    for (var i = 0; i < times; i++) {
+        res += str
+    }
+    return res
 }
 
 console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+// console.log(repeat('a', 5));
+
+
+
+
+
+
