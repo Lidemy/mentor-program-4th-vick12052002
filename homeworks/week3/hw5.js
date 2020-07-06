42588aa2edd35185218577cb8adbe1abb98bf4eb
ali <vick12052002@gmail.com>
2020-07-06 17:36:51 +0800

    hw5 c.lenrth way

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -14,28 +14,45 @@ function solve(input) {
   let arr = [];
   let a = 0;
   let b = 0;
+  function numberIsBigger(c, d) {
+    const cStr = c.toString();
+    const dStr = d.toString();
+    const same = 'DRWA';
+    if (cStr.length > dStr.length) {
+      return true;
+    } if (cStr.length === dStr.length) {
+      for (let i = 0; i < c.length; i += 1) {
+        if (cStr[i] > dStr[i]) { return true; }
+        if (cStr[c.length - 1] === dStr[d.length - 1]) { return same; }
+      }
+    }
+    return false;
+  }
+
   function printBig(numOne, numTwo) {
-    if (numOne > numTwo) {
+    if (numberIsBigger(numOne, numTwo) === 'DRWA') {
+      return ('DRWA');
+    } if (numberIsBigger(numOne, numTwo)) {
       return ('A');
     }
     return ('B');
   }
   function printSmall(x, z) {
-    if (x < z) {
-      return ('A');
+    if (numberIsBigger(x, z) === 'DRWA') {
+      return ('DRWA');
+    } if (numberIsBigger(x, z)) {
+      return ('B');
     }
-    return ('B');
+    return ('A');
   }
   for (let i = 1; i <= m; i += 1) {
     arr = input[i].toString().split(' ');
 
-    a = Number(arr[0]);
-    b = Number(arr[1]);
+    a = (arr[0]).toString();
+    b = (arr[1]).toString();
 
     if (Number(arr[2]) === 1) {
       console.log(printBig(a, b));
-    } else if (a === b) {
-      console.log('DRAW');
     } else {
       console.log(printSmall(a, b));
     }
