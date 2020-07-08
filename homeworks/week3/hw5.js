4fb7b1de767091ca3037950826f0fbb9475e3355
ali <vick12052002@gmail.com>
2020-07-08 19:06:59 +0800

    w3 ok

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -8,50 +8,32 @@ const rl = readline.createInterface({
 rl.on('line', (line) => {
   lines.push(line);
 });
-
 function solve(input) {
   const m = input[0];
-  let arr = [];
-  function numberIsBigger(c, d) {
-    const cStr = c.toString();
-    const dStr = d.toString();
-    const same = 'DRWA';
-    if (cStr === dStr) { return same; }
-    if (cStr.length > dStr.length) { return true; }
-    if (cStr.length === dStr.length) {
-      for (let i = 0; i < cStr.length; i += 1) {
-        if (cStr[i] > dStr[i]) { return true; }
-      }
-    }
+  function IsBigger(c, d) {
+    const same = 'DRAW';
+    if (c === d) { return same; }
+    if (c.length > d.length) { return true; }
+    if (c.length === d.length) { return c > d; }
     return false;
   }
-
-  function printBig(numOne, numTwo) {
-    if (numberIsBigger(numOne, numTwo) === 'DRWA') {
-      return ('DRWA');
-    } if (numberIsBigger(numOne, numTwo)) {
-      return ('A');
-    }
-    return ('B');
+  function printBig(z, x) {
+    if (IsBigger(z, x) !== 'DRAW') { return IsBigger(z, x) ? 'A' : 'B'; }
+    return 'DRAW';
   }
-  function printSmall(x, z) {
-    if (numberIsBigger(x, z) === 'DRWA') {
-      return ('DRWA');
-    } if (numberIsBigger(x, z)) {
-      return ('B');
-    }
-    return ('A');
+
+  function printSmall(k, g) {
+    if (IsBigger(k, g) !== 'DRAW') { return IsBigger(k, g) ? 'B' : 'A'; }
+    return 'DRAW';
   }
   for (let i = 1; i <= m; i += 1) {
-    arr = input[i].toString().split(' ');
+    const arr = input[i].split(' ');
     const a = arr[0];
     const b = arr[1];
-
-    if (Number(arr[2]) === 1) {
+    const c = arr[2];
+    if (Number(c) === 1) {
       console.log(printBig(a, b));
-    } else {
-      console.log(printSmall(a, b));
-    }
+    } else { console.log(printSmall(a, b)); }
   }
 }
 
